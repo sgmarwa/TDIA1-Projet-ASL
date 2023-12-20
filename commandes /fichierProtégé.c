e9200b0eedff9d951a9240a069911a9e5b6d532f
sgmarwa <154100981+sgmarwa@users.noreply.github.com>
2023-12-20 03:30:15 +0100

    Update fichierProtégé.c

--- commandes /fichierProtégé.c
+++ commandes /fichierProtégé.c
@@ -5,48 +5,47 @@
 #include <string.h>
 
 #define MAX_PASSWORD_LENGTH 50
-#define FILENAME "fichier_protecte.txt"
 
-void createProtectedFile() {
+void creerFichierProtege(const char *nomFichier) {
     int fd;
-    char password[MAX_PASSWORD_LENGTH];
+    char motDePasse[MAX_PASSWORD_LENGTH];
 
-    printf("Entrez le mot de passe pour le fichier : ");
-    fgets(password, sizeof(password), stdin);
-    password[strcspn(password, "\n")] = '\0'; // Supprimer le retour à la ligne
+    printf("Entrez le mot de passe pour le fichier '%s' : ", nomFichier);
+    fgets(motDePasse, sizeof(motDePasse), stdin);
+    motDePasse[strcspn(motDePasse, "\n")] = '\0'; // Supprimer le retour à la ligne
 
-    fd = open(FILENAME, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+    fd = open(nomFichier, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
     if (fd == -1) {
         perror("Erreur lors de la création du fichier");
         exit(EXIT_FAILURE);
     }
 
-    write(fd, password, strlen(password));
+    write(fd, motDePasse, strlen(motDePasse));
     close(fd);
 
     printf("Fichier créé avec succès.\n");
 }
 
-void unlockFile() {
+void deverouillerFichier(const char *nomFichier) {
     int fd;
-    char storedPassword[MAX_PASSWORD_LENGTH];
-    char enteredPassword[MAX_PASSWORD_LENGTH];
+    char motDePasseStocke[MAX_PASSWORD_LENGTH];
+    char motDePasseEntree[MAX_PASSWORD_LENGTH];
 
-    fd = open(FILENAME, O_RDONLY);
+    fd = open(nomFichier, O_RDONLY);
     if (fd == -1) {
         perror("Erreur lors de l'ouverture du fichier");
         exit(EXIT_FAILURE);
     }
 
-    read(fd, storedPassword, sizeof(storedPassword));
+    read(fd, motDePasseStocke, sizeof(motDePasseStocke));
     close(fd);
 
-    printf("Entrez le mot de passe pour accéder au fichier : ");
-    fgets(enteredPassword, sizeof(enteredPassword), stdin);
-    enteredPassword[strcspn(enteredPassword, "\n")] = '\0'; // Supprimer le retour à la ligne
+    printf("Entrez le mot de passe pour accéder au fichier '%s' : ", nomFichier);
+    fgets(motDePasseEntree, sizeof(motDePasseEntree), stdin);
+    motDePasseEntree[strcspn(motDePasseEntree, "\n")] = '\0'; // Supprimer le retour à la ligne
 
-    if (strcmp(storedPassword, enteredPassword) == 0) {
-        printf("Mot de passe correct. Vous pouvez accéder au fichier.\n");
+    if (strcmp(motDePasseStocke, motDePasseEntree) == 0) {
+        printf("Mot de passe correct. Vous pouvez accéder au fichier '%s'.\n", nomFichier);
         // Ajoutez ici le code pour effectuer des opérations sur le fichier, par exemple cat, nano, etc.
     } else {
         printf("Mot de passe incorrect. Accès refusé.\n");
@@ -54,17 +53,20 @@ void unlockFile() {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "La formule correcte est : %s <create/unlock>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "La formule correcte est : %s <creer/deverouiller> <nomFichier>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if (strcmp(argv[1], "create") == 0) {
-        createProtectedFile();
-    } else if (strcmp(argv[1], "unlock") == 0) {
-        unlockFile();
+    const char *commande = argv[1];
+    const char *nomFichier = argv[2];
+
+    if (strcmp(commande, "creer") == 0) {
+        creerFichierProtege(nomFichier);
+    } else if (strcmp(commande, "deverouiller") == 0) {
+        deverouillerFichier(nomFichier);
     } else {
-        fprintf(stderr, "Commande non reconnue. Utilisez 'create' pour créer un fichier ou 'unlock' pour le déverrouiller.\n");
+        fprintf(stderr, "Commande non reconnue. Utilisez 'creer' pour créer un fichier ou 'deverouiller' pour le déverrouiller.\n");
         exit(EXIT_FAILURE);
     }
 
