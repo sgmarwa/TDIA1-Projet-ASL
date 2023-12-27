c17372ffea4d78d38fcb85888036131fd169510d
mourinou <154349139+mourinou@users.noreply.github.com>
2023-12-27 07:37:10 +0100

    Update loggin.h

--- commandes /loggin.h
+++ commandes /loggin.h
@@ -48,3 +48,45 @@ nombreLignes++;
     // Fermer le fichier journal
     fclose(logFile);
 }
+void logMessage2(const char *type,const char *message,usr us ) {
+// Ouvrir le fichier en mode lecture
+    FILE *fichier = fopen("logfile.txt", "r");
+
+    // Vérifier si le fichier est ouvert avec succès
+    if (fichier == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+    }
+
+    // Compter le nombre de lignes dans le fichier
+    int nombreLignes = 0;
+    char caractere;
+
+    while ((caractere = fgetc(fichier)) != EOF) {
+        if (caractere == '\n') {
+            nombreLignes++;
+        }
+    }
+nombreLignes++;
+    // Fermer le fichier
+    fclose(fichier);
+    FILE *logFile = fopen("logfile.txt", "a");
+    if (logFile == NULL) {
+        perror("Erreur lors de l'ouverture du fichier journal");
+    }
+
+    // Obtenir la date et l'heure actuelles
+    time_t rawtime;
+    struct tm *timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    // Formater le message avec la date et l'heure
+    char timestamp[20];
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeinfo);
+
+    // Écrire le message dans le fichier journal
+    fprintf(logFile, "[%d] %s :%s: [%s] :%s\n", nombreLignes,type,us.nom_uti,timestamp,message);
+    // Fermer le fichier journal
+    fclose(logFile);
+}
+
