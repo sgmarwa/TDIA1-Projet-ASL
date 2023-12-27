9dddff20df79561d5b0d761b18453e8ecd21e173
mourinou <154349139+mourinou@users.noreply.github.com>
2023-12-27 07:18:07 +0100

    Update position(pwd).c

--- commandes /position(pwd).c
+++ commandes /position(pwd).c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include "loggin.h"
 int main() {
     char cwd[1024];
     
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
         printf("Position: %s\n", cwd);
+        logMessage2("DEBUG","utilisation du commande <position>",usr us );
     } else {
         perror("Erreur lors de la récupération du répertoire actuel");
         return 1;
