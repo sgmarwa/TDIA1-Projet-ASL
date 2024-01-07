0cdbbedf3c1cc95e8e0854ec8f9cffcc2790e077
mourinou <154349139+mourinou@users.noreply.github.com>
2024-01-07 13:14:42 +0100

    Update position(pwd).c

--- commandes /position(pwd).c
+++ commandes /position(pwd).c
@@ -4,18 +4,18 @@
 #include "usr.h"
 int main() {
     char utilisateur[50];
-FILE *f3 = fopen("connecteur.txt", "r");
+/*FILE *f3 = fopen("connecteur.txt", "r");
     if (f3 == NULL) {
         perror("Erreur lors de l'ouverture du fichier");
         return 1;
     }
 fgets(utilisateur, sizeof(utilisateur), f3);
-fclose(f3);
+fclose(f3);*/
     char cwd[1024];
     
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
         printf("Position: %s\n", cwd);
-        logMessage2("DEBUG","utilisation du commande <position>",utilisateur);
+     //   logMessage2("DEBUG","utilisation du commande <position>",utilisateur);
     } else {
         perror("Erreur lors de la récupération du répertoire actuel");
         return 1;
